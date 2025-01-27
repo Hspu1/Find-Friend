7442090a165797813a21e3c55d8bdede0414f1fc
Макс <rumyantsev1m0s1@gmail.com>
2025-01-27 16:39:25 +0300

    Начал писать следующую логику - страница с кнопкой, после нажатия которой пользователь переходит на страницу авторизации через гугл

---
+++ app/google_auth/routs.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+google_auth_router = APIRouter(
+    prefix="google_auth",
+    tags=["Google Auth"]
+)
+
+
+
--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from datetime import date
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import HTMLResponse, ORJSONResponse
 from fastui import FastUI, AnyComponent, prebuilt_html, components as c
+from fastui.components import Link
 from fastui.components.display import DisplayMode, DisplayLookup
 from fastui.events import GoToEvent, BackEvent
 from pydantic import BaseModel, Field
@@ -14,58 +15,69 @@ app = FastAPI(
 )
 
 
-class User(BaseModel):
-    id: int
-    name: str
-    dob: date = Field(title='Date of Birth')
-
-
-users = [
-    User(id=1, name='John', dob=date(1990, 1, 1)),
-    User(id=2, name='Jack', dob=date(1991, 1, 1)),
-    User(id=3, name='Jill', dob=date(1992, 1, 1)),
-    User(id=4, name='Jane', dob=date(1993, 1, 1)),
-]
+# class User(BaseModel):
+#     id: int
+#     name: str
+#     dob: date = Field(title='Date of Birth')
+#
+#
+# users = [
+#     User(id=1, name='John', dob=date(1990, 1, 1)),
+#     User(id=2, name='Jack', dob=date(1991, 1, 1)),
+#     User(id=3, name='Jill', dob=date(1992, 1, 1)),
+#     User(id=4, name='Jane', dob=date(1993, 1, 1)),
+# ]
 
 
 @app.get("/api/", response_model=FastUI, response_model_exclude_none=True)
 def users_table() -> list[AnyComponent]:
+    # return [
+    #     c.Page(
+    #         components=[
+    #             c.Heading(text='Users', level=2),
+    #             c.Table(
+    #                 data=users,
+    #                 columns=[
+    #                     DisplayLookup(field='name', on_click=GoToEvent(url='/user/{id}/')),
+    #                     DisplayLookup(field='dob', mode=DisplayMode.date),
+    #                 ],
+    #             ),
+    #         ]
+    #     ),
+    # ]
     return [
         c.Page(
             components=[
-                c.Heading(text='Users', level=2),
-                c.Table(
-                    data=users,
-                    columns=[
-                        DisplayLookup(field='name', on_click=GoToEvent(url='/user/{id}/')),
-                        DisplayLookup(field='dob', mode=DisplayMode.date),
-                    ],
-                ),
-            ]
-        ),
+                c.Button(
+                    text='Google',
+                    on_click=Link(url='/google_auth_login')
+                )
+            ],
+            title='Google Auth'
+        )
     ]
 
 
-@app.get("/api/user/{user_id}/", response_model=FastUI, response_model_exclude_none=True)
-def user_profile(user_id: int) -> list[AnyComponent]:
-    try:
-        user = next(u for u in users if u.id == user_id)
-    except StopIteration:
-        raise HTTPException(status_code=404, detail="User not found")
-    return [
-        c.Page(
-            components=[
-                c.Heading(text=user.name, level=2),
-                c.Link(components=[c.Text(text='Back')], on_click=BackEvent()),
-                c.Details(data=user),
-            ]
-        ),
-    ]
-
-
-@app.get('/{path:path}')
-async def html_landing() -> HTMLResponse:
-    return HTMLResponse(prebuilt_html(title='FastUI Demo'))
+# @app.get("/api/user/{user_id}/", response_model=FastUI, response_model_exclude_none=True)
+# def user_profile(user_id: int) -> list[AnyComponent]:
+#     try:
+#         user = next(u for u in users if u.id == user_id)
+#     except StopIteration:
+#         raise HTTPException(status_code=404, detail="User not found")
+#     return [
+#         c.Page(
+#             components=[
+#                 c.Heading(text=user.name, level=2),
+#                 c.Link(components=[c.Text(text='Back')], on_click=BackEvent()),
+#                 c.Details(data=user),
+#             ]
+#         ),
+#     ]
+#
+#
+# @app.get('/{path:path}')
+# async def html_landing() -> HTMLResponse:
+#     return HTMLResponse(prebuilt_html(title='FastUI Demo'))
 
 
 if __name__ == '__main__':
