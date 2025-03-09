661a3989d64dd391a482ed403acdcb00f8c86492
Макс <rumyantsev1m0s1@gmail.com>
2025-03-09 20:50:36 +0300

    Изменение: откат назад, сделал новые credentials для авторизации так как до этого всё работало (локально хыхы)

--- app/backend/submit_password.py
+++ app/backend/submit_password.py
@@ -1,6 +1,5 @@
 import bcrypt
 from fastapi import Form, APIRouter, HTTPException
-from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 
 from app.core import async_session_maker, AuthModel
@@ -11,23 +10,15 @@ submit_password_router = APIRouter()
 
 @submit_password_router.post(path="/submit_password", status_code=201)
 async def submit_password(username: str = Form(...), password: str = Form(...)):
-    async with async_session_maker() as session:
-        result = await session.execute(
-            select(AuthModel).where(AuthModel.username == username)
-        )
-        existing_user = result.scalar_one_or_none()
-
-        if existing_user:
-            raise HTTPException(status_code=409, detail="Пользователь с таким именем уже существует")
-
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     str_hashed_password = hashed_password.decode("utf-8")
 
     new_user_data = AuthModel(username=username, hashed_psw=str_hashed_password)
+
     try:
         async with async_session_maker() as session:
             async with session.begin():
                 session.add(new_user_data)
 
     except IntegrityError:
-        raise HTTPException(status_code=500, detail="Ошибка при добавлении пользователя")
+        raise HTTPException(status_code=409)
