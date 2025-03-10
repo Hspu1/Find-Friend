618787106ba5d904ef039aac28b012ef3f4ccf93
Макс <rumyantsev1m0s1@gmail.com>
2025-03-10 18:22:12 +0300

    Изменение: добавил асинхронный движок в ссылку на БД

--- alembic/env.py
+++ alembic/env.py
@@ -15,9 +15,8 @@ from app.core import (
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option(
-    "sqlalchemy.url", db_url
-)
+config.set_main_option("sqlalchemy.url", db_url)
+
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
