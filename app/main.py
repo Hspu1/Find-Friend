0355c8d48b689a3bdc807fde779d13944060f3a8
Макс <rumyantsev1m0s1@gmail.com>
2025-03-10 18:55:37 +0300

    Изменение: добавлена логика програмной прогонки миграций

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import os
 
+from alembic import command, config
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from starlette.middleware.cors import CORSMiddleware
@@ -57,6 +58,10 @@ app.include_router(showing_questionnaires)
 
 
 if __name__ == "__main__":
+    alembic_cfg = config.Config("alembic.ini")
+    alembic_cfg.set_main_option("sqlalchemy.url", os.getenv("DB_URL"))
+
+    command.upgrade(alembic_cfg, "head")
     run(
         app="app.main:app", host="0.0.0.0",
         port=80, reload=True, use_colors=True
